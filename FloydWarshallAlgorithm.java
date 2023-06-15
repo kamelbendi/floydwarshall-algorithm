5c858c12c8ae398f6b07c0c16339be2000eb3cc1
Kamel BENDIMERAD <kamel.bendimerad@gmail.com>
2023-06-15 18:08:14 +0200

    final

--- FloydWarshallAlgorithm.java
+++ FloydWarshallAlgorithm.java
@@ -1,138 +1,111 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class FloydWarshallAlgorithm {
+    private static final int INF = Integer.MAX_VALUE;
 
-    private static int numNodes;
-    private static int numEdges;
-    private static int numRoutes;
-    private static int numIterations;
+    public static void main(String[] args) {
+        String inputFile = "g1000.txt";
+        String outputFile = "g1000_out.txt";
 
-    private static class Edge {
-        int source;
-        int target;
-        int weight;
+        int[][] graph = readGraph(inputFile);
+        int[][] distanceMatrix = performFloydWarshall(graph);
 
-        Edge(int source, int target, int weight) {
-            this.source = source;
-            this.target = target;
-            this.weight = weight;
-        }
+        writeOutputToFile(outputFile, graph, distanceMatrix);
     }
 
-    private static int[][] readGraphFromFile(String filename) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File(filename));
-        numNodes = scanner.nextInt();
-        numEdges = scanner.nextInt();
+    public static int[][] readGraph(String inputFile) {
+        int[][] graph = null;
+        try (Scanner scanner = new Scanner(new File(inputFile))) {
+            int nodes = scanner.nextInt();
+            int edges = scanner.nextInt();
 
-        int[][] graph = new int[numNodes][numNodes];
-        for (int i = 0; i < numNodes; i++) {
-            for (int j = 0; j < numNodes; j++) {
-                graph[i][j] = (i == j) ? 0 : Integer.MAX_VALUE;
-            }
-        }
-
-        for (int i = 0; i < numEdges; i++) {
-            int source = scanner.nextInt() - 1;
-            int target = scanner.nextInt() - 1;
-            int weight = scanner.nextInt();
-            graph[source][target] = weight;
-        }
-
-        scanner.close();
-        return graph;
-    }
-
-    private static void writeOutputToFile(String filename, int[][] graph) throws FileNotFoundException {
-        PrintWriter writer = new PrintWriter(new File(filename));
-        for (int i = 0; i < numNodes; i++) {
-            for (int j = 0; j < numNodes; j++) {
-                if (graph[i][j] == Integer.MAX_VALUE) {
-                    writer.printf("d[%d,%d] = INF PATH: No path\n", i + 1, j + 1);
-                } else {
-                    writer.printf("d[%d,%d] = %d PATH: %d", i + 1, j + 1, graph[i][j], i + 1);
-                    printPath(j, i, graph, writer);
-                    writer.println();
-                }
-            }
-        }
-        writer.close();
-    }
-
-    private static void printPath(int current, int source, int[][] graph, PrintWriter writer) {
-        if (current != source) {
-            int next = graph[current][source];
-            printPath(next, source, graph, writer);
-            writer.printf("-%d", current + 1);
-        }
-    }
-
-    private static void floydWarshall(int[][] graph) {
-        int[][] dist = new int[numNodes][numNodes];
-        int[][] next = new int[numNodes][numNodes];
-
-        for (int i = 0; i < numNodes; i++) {
-            for (int j = 0; j < numNodes; j++) {
-                dist[i][j] = graph[i][j];
-                if (dist[i][j] != Integer.MAX_VALUE && i != j) {
-                    next[i][j] = j;
-                } else {
-                    next[i][j] = -1;
-                }
-            }
-        }
-
-        numIterations = 0;
-        for (int k = 0; k < numNodes; k++) {
-            for (int i = 0; i < numNodes; i++) {
-                for (int j = 0; j < numNodes; j++) {
-                    if (dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE
-                            && dist[i][k] + dist[k][j] < dist[i][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                        next[i][j] = next[i][k];
+            graph = new int[nodes][nodes];
+            for (int i = 0; i < nodes; i++) {
+                for (int j = 0; j < nodes; j++) {
+                    if (i == j) {
+                        graph[i][j] = 0;
+                    } else {
+                        graph[i][j] = INF;
                     }
                 }
             }
-            numIterations++;
-        }
 
-        numRoutes = countRoutes(next);
+            for (int k = 0; k < edges; k++) {
+                int source = scanner.nextInt() - 1;
+                int destination = scanner.nextInt() - 1;
+                int weight = scanner.nextInt();
+
+                graph[source][destination] = weight;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return graph;
     }
 
-    private static int countRoutes(int[][] next) {
-        int count = 0;
-        for (int i = 0; i < numNodes; i++) {
-            for (int j = 0; j < numNodes; j++) {
-                if (i != j && next[i][j] != -1) {
-                    count++;
+    public static int[][] performFloydWarshall(int[][] graph) {
+        int nodes = graph.length;
+        int[][] distance = new int[nodes][nodes];
+
+        for (int i = 0; i < nodes; i++) {
+            for (int j = 0; j < nodes; j++) {
+                distance[i][j] = graph[i][j];
+            }
+        }
+
+        for (int k = 0; k < nodes; k++) {
+            for (int i = 0; i < nodes; i++) {
+                for (int j = 0; j < nodes; j++) {
+                    if (distance[i][k] != INF && distance[k][j] != INF
+                            && distance[i][k] + distance[k][j] < distance[i][j]) {
+                        distance[i][j] = distance[i][k] + distance[k][j];
+                    }
                 }
             }
         }
-        return count;
+
+        return distance;
     }
 
-    public static void main(String[] args) {
-        try {
-            String inputFile = "g5.txt";
-            String outputFile = "g5_out.txt";
+    public static void writeOutputToFile(String outputFile, int[][] graph, int[][] distanceMatrix) {
+        try (FileWriter writer = new FileWriter(outputFile)) {
+            int nodes = graph.length;
+            int edges = countEdges(graph);
+            int routes = nodes * (nodes - 1);
 
-            int[][] graph = readGraphFromFile(inputFile);
+            writer.write("Number of Nodes: " + nodes + "\n");
+            writer.write("Number of Edges: " + edges + "\n");
+            writer.write("Number of Routes: " + routes + "\n");
+            writer.write("Number of Iterations: " + nodes + "\n\n");
 
-            long startTime = System.currentTimeMillis();
-            floydWarshall(graph);
-            long endTime = System.currentTimeMillis();
-
-            writeOutputToFile(outputFile, graph);
-
-            System.out.println("Number of Nodes: " + numNodes);
-            System.out.println("Number of Edges: " + numEdges);
-            System.out.println("Number of Routes: " + numRoutes);
-            System.out.println("Iterations: " + numIterations);
-            System.out.println("Execution Time: " + (endTime - startTime) + " milliseconds");
-        } catch (FileNotFoundException e) {
+            for (int i = 0; i < nodes; i++) {
+                for (int j = 0; j < nodes; j++) {
+                    writer.write("d[" + (i + 1) + "," + (j + 1) + "] = ");
+                    if (distanceMatrix[i][j] == INF) {
+                        writer.write("INF");
+                    } else {
+                        writer.write(Integer.toString(distanceMatrix[i][j]));
+                    }
+                    writer.write("\n");
+                }
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static int countEdges(int[][] graph) {
+        int edges = 0;
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph.length; j++) {
+                if (graph[i][j] != 0 && graph[i][j] != INF) {
+                    edges++;
+                }
+            }
+        }
+        return edges;
     }
 }
